e1fae55fff4aa47be67582eec9d16a9cab759f89
apriyanto2804 <craziyuhuu@gmail.com>
2024-08-27 11:44:01 +0700

    Menambahkan kode Python untuk proses pengujian

---
+++ codePython/Pengujian.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+import glob
+from PIL import Image
+import time
+
+def get_image_files(directory):
+    image_extensions = ('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff')
+    image_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(image_extensions):
+                image_files.append(os.path.join(root, file))
+    return image_files
+
+## Lokasi Citra
+citra_awal = ""
+citra_hasil_deblurring = ""
+citra_hasil_denoising = ""
+
+## Mendapatkan nama file citra
+arr_citra_awal = get_image_files(citra_awal)
+arr_citra_hasil_deblurring = get_image_files(citra_hasil_deblurring)
+arr_citra_hasil_denoising = get_image_files(citra_hasil_denoising)
